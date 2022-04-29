8dea57b6ef17469de6d69bb87ec1f47a2ba02748
andreawatkins <awatki22@depaul.edu>
2022-04-28 22:52:29 -0500

    Access Media info and populate on media page

--- src/pages/MediaPage.js
+++ src/pages/MediaPage.js
@@ -2,10 +2,13 @@ import axios from 'axios'
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
+import {useState} from 'react'
+
+
 
 function MediaPage() {
-    let params = useParams()
-    return <h1>Movie Id: {params.movieId}</h1>
+   let params = useParams()
+    
     /*
     async function fetchData() {
     let fetchUrl = `https://api.themoviedb.org/3/movie/${params.movieId}?${process.env.REACT_APP_APIKEY}=&language=en-US`
@@ -18,6 +21,22 @@ function MediaPage() {
 
     }
     */
+   const [movie, setMovie] = useState('WAITING FOR DATA')
+    const getMovieInfo = async() =>{
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${params.movieId}?api_key=${process.env.REACT_APP_APIKEY}&language=en-US`) 
+    const data = await response.json()
+    setMovie(data)
+    console.log(movie)
+    console.log(data)
+    return data
+ }
+
+getMovieInfo()
+    
+    let string = `Title: ${movie.title}, Id: ${params.movieId}, ${movie.overview} `
+     return <h1> {string} </h1>
 }
+
+
 
 export default MediaPage
