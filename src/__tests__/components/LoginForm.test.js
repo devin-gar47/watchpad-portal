34911ee4f9a3d1e8c238bb2c9b614ea9e05ee381
devin-gar47 <devingar18@gmail.com>
2022-05-02 11:56:33 -0500

    Added additional tests for submit form

--- src/__tests__/components/LoginForm.test.js
+++ src/__tests__/components/LoginForm.test.js
@@ -12,9 +12,23 @@ const myStore = configureStore({
     devTools: true,
 })
 
-let getByTestIdFunc
+it('should trigger email onchange event', () => {
+    const setIsLoadingMock = jest.fn()
+    const history = createMemoryHistory()
+    const { getByTestId } = render(
+        <Provider store={myStore}>
+            <Router location={history.location} navigator={history}>
+                <LoginForm setIsLoading={setIsLoadingMock} />
+            </Router>
+        </Provider>
+    )
+    const emailInputBox = getByTestId('login-form-email-input-box')
+    expect(emailInputBox.value).toEqual('')
+    fireEvent.change(emailInputBox, { target: { value: 'test@gmail.com' } })
+    expect(emailInputBox.value).toEqual('test@gmail.com')
+})
 
-beforeAll(() => {
+it('should trigger password change event', () => {
     const setIsLoadingMock = jest.fn()
     const history = createMemoryHistory()
     const { getByTestId } = render(
@@ -25,24 +39,23 @@ beforeAll(() => {
         </Provider>
     )
 
-    getByTestIdFunc = getByTestId
-})
-
-it('should trigger email onchange event', () => {
-    const emailInputBox = getByTestIdFunc('login-form-email-input-box')
-    expect(emailInputBox.value).toEqual('')
-    fireEvent.change(emailInputBox, { target: { value: 'test@gmail.com' } })
-    expect(emailInputBox.value).toEqual('test@gmail.com')
-})
-
-it('should trigger password change event', () => {
-    const passwordInputBox = getByTestIdFunc('login-form-password-input-box')
+    const passwordInputBox = getByTestId('login-form-password-input-box')
     expect(passwordInputBox.value).toEqual('')
     fireEvent.change(passwordInputBox, { target: { value: 'password' } })
     expect(passwordInputBox.value).toEqual('password')
 })
 
 it('should trigger form handleSubmit function', () => {
-    const form = getByTestIdFunc('login-form')
+    const setIsLoadingMock = jest.fn()
+    const history = createMemoryHistory()
+    const { getByTestId } = render(
+        <Provider store={myStore}>
+            <Router location={history.location} navigator={history}>
+                <LoginForm setIsLoading={setIsLoadingMock} />
+            </Router>
+        </Provider>
+    )
+
+    const form = getByTestId('login-form')
     fireEvent.submit(form)
 })
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,172 +1,23 @@
-import axios from 'axios'
 import React, { useState } from 'react'
-import { Alert, Button, Col, Form, Row } from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
+import { Col, Row } from 'react-bootstrap'
+import SignupForm from '../components/SignupForm'
+import Loading from '../components/Loading'
 
 function SignupPage() {
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [error, setError] = useState('')
-    const [usernameValidation, setUsernameValidation] = useState('')
-    const [emailValidation, setEmailValidation] = useState('')
-    const [passwordValidation, setPasswordValidation] = useState('')
-    let navigate = useNavigate()
-
-    function isValid(userInfoObj) {
-        const { username, email, password, confirmPassword } = userInfoObj
-        let valid = true
-
-        if (!username) {
-            setUsernameValidation('Username cannot be blank.')
-            valid = false
-        }
-        if (!email) {
-            setEmailValidation('Email cannot be blank.')
-            valid = false
-        }
-        if (!password) {
-            setPasswordValidation('Password cannot be blank.')
-            valid = false
-        }
-        if (password !== confirmPassword) {
-            setPasswordValidation('Passwords must match.')
-            valid = false
-        }
-        return valid
-    }
-
-    function handleChange(e) {
-        e.preventDefault()
-        setEmailValidation('')
-        setUsernameValidation('')
-        setPasswordValidation('')
-        const { name, value } = e.target
-        switch (name) {
-            case 'username':
-                setUsername(value)
-                break
-            case 'email':
-                setEmail(value)
-                break
-            case 'password':
-                setPassword(value)
-                break
-            case 'confirmPassword':
-                setConfirmPassword(value)
-                break
-            default:
-                return
-        }
-    }
-
-    async function handleSubmit(e) {
-        e.preventDefault()
-        const userInfoObj = {
-            username,
-            email,
-            password,
-            confirmPassword,
-        }
-        if (!isValid(userInfoObj)) return
-
-        try {
-            await axios.post(
-                `${process.env.REACT_APP_API_BASE_URL}/api/user/sign-up`,
-                userInfoObj
-            )
-            navigate('/login')
-        } catch (err) {
-            if (!err.response) {
-                setError('Server currently is not running.')
-                return
-            }
-            const errorMessage = err?.response?.data
-            setError(errorMessage)
-        }
-    }
+    const [isLoading, setIsLoading] = useState(false)
 
     return (
         <>
             <Row className="initial-page-height justify-content-center align-items-center bg-light bg-gradient">
                 <Col
                     lg={6}
-                    className="shadow p-5 rounded justify-content-center align-items-center"
+                    className="shadow p-5 rounded justify-content-center align-items-center text-center"
                 >
-                    <Alert variant="danger" hidden={!error}>
-                        {error}
-                    </Alert>
-                    <Form
-                        className="text-center"
-                        onSubmit={(e) => handleSubmit(e)}
-                    >
-                        <h4 className="mb-3">Create an Account</h4>
-                        <Row className="mb-3">
-                            <Col>
-                                <Form.Control
-                                    name="username"
-                                    type="text"
-                                    placeholder="Username"
-                                    value={username}
-                                    onChange={(e) => handleChange(e)}
-                                />
-                                <Form.Text
-                                    className="text-danger"
-                                    hidden={!usernameValidation}
-                                >
-                                    {usernameValidation}
-                                </Form.Text>
-                            </Col>
-                            <Col>
-                                <Form.Control
-                                    name="email"
-                                    type="email"
-                                    placeholder="Email"
-                                    value={email}
-                                    onChange={(e) => handleChange(e)}
-                                />
-                                <Form.Text
-                                    className="text-danger"
-                                    hidden={!emailValidation}
-                                >
-                                    {emailValidation}
-                                </Form.Text>
-                            </Col>
-                        </Row>
-                        <Row className="mb-3">
-                            <Col>
-                                <Form.Control
-                                    name="password"
-                                    type="password"
-                                    placeholder="Password"
-                                    value={password}
-                                    onChange={(e) => handleChange(e)}
-                                />
-                                <Form.Text
-                                    className="text-danger"
-                                    hidden={!passwordValidation}
-                                >
-                                    {passwordValidation}
-                                </Form.Text>
-                            </Col>
-                            <Col>
-                                <Form.Control
-                                    name="confirmPassword"
-                                    type="password"
-                                    placeholder="Confirm Password"
-                                    value={confirmPassword}
-                                    onChange={(e) => handleChange(e)}
-                                />
-                            </Col>
-                        </Row>
-                        <Button className="text-center" type="submit">
-                            Sign Up
-                        </Button>
-                        <Link to="/login">
-                            <p className="mt-3">Have an account already?</p>
-                        </Link>
-                    </Form>
+                    {!isLoading ? (
+                        <SignupForm setIsLoading={setIsLoading} />
+                    ) : (
+                        <Loading message="Attempting to sign you up..." />
+                    )}
                 </Col>
             </Row>
         </>
