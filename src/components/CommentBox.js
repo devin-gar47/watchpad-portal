8ea0db2964d2fd56019a1c922e804a7741dbcea6
andreawatkins <awatki22@depaul.edu>
2022-05-11 02:51:47 -0500

    add code to connect comment backend, add proxy to fix CORS

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,34 +1,48 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import './CommentBox.css'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function CommentBox() {
-    const [comments, setComments] = useState([]) //state that stores the new inputs in an array
-    const [input, setInput] = useState('') //short term memory to remember what is typed into input area(text area)
+    let params = useParams();
+    const userInformation = useSelector((store) => store.userInformation)
+
+    const [comments, setComments] = useState([]); //state that stores the new inputs in an array
+    const [newComment, setNewComment] = useState("") //short term memory to remember what is typed into input area(text area)
 
     const addComment = (event) => {
+        axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/comments`, {
+        
+            media: params,
+            user: userInformation.id,
+            comment_timestamp:"2007-12-03T10:15:30",
+            content: newComment,
+            spoiler: true,
+        })
+        .then((response) => {console.log("Comment Added!")});
         // this will activate when submit button is clicked
-        setComments([...comments, input]) // ...spread. pushes the new input into the array. keeps what is already inside array and appends new input
+        setComments([...comments, newComment]) // ...spread. pushes the new input into the array. keeps what is already inside array and appends new input
         event.preventDefault() // prevents the page refresh when Submit
-        setInput('') //clears the text area back to empty after submitting
+        setNewComment('') //clears the text area back to empty after submitting
     }
 
+    
     return (
         <div className="commentBox">
             <br />
 
             <div>
                 <form>
-                    {comments.map((comment) => (
-                        <p>{comment}</p> //loops(map) through array of useState and prints each element in the array then returns list<li> or <p> items of elements in the array
-                    ))}
+                    
 
                     <textarea
                         type="text"
                         style={{ width: '100%' }}
                         placeholder="Write your comment here..."
-                        value={input} //mapping state input(above) into input that is being filled in text area
-                        onChange={(event) => setInput(event.target.value)} //every time user types, state gets updated above. target = text area field you type in
+                        value={newComment} //mapping state input(above) into input that is being filled in text area
+                        onChange={(event) => setNewComment(event.target.value)} //every time user types, state gets updated above. target = text area field you type in
                     ></textarea>
                     <br />
 
@@ -37,12 +51,13 @@ function CommentBox() {
                         type="submit"
                         variant="primary"
                     >
-                        Submit
+                        Post
                     </Button>
                 </form>
             </div>
             <br />
         </div>
+        
     )
 }
 
