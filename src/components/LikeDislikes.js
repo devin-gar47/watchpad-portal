9b847e2f61e5cf3b153523cb22d935640ce1a796
Ashlyn Harrington <ashlyn.harrington@gmail.com>
2022-05-15 19:04:33 -0500

    can create, read, update, delete likes and dislikes on media with responsive front-end

--- src/components/LikeDislikes.js
+++ src/components/LikeDislikes.js
@@ -1,26 +1,99 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-function LikeDislikes({ likeCount, dislikeCount }) {
+function LikeDislikes() {
+    const userInformation = useSelector((store) => store.userInformation)
+    let params = useParams()
+
     //used props to make it dynamic instead of setting state to 0
-    const [likes, setLikes] = useState(likeCount)
-    const [dislikes, setDislikes] = useState(dislikeCount)
+    const [likes, setLikes] = useState(0)
+    const [dislikes, setDislikes] = useState(0)
 
     //another state used to track if the like/dislike button is already pressed
-    const [liked, updateLiked] = useState(false)
-    const [disliked, updateDisliked] = useState(false)
+    const [liked, updateLiked] = useState(0)
+    const [disliked, updateDisliked] = useState(0)
 
-    let params = useParams()
-    const userInformation = useSelector((store) => store.userInformation)
+    const getTotalLikes = async () => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/count-likes?mediaId=${params.movieId}`
+        )
+        setLikes(response.data)
+    }
+
+    const getTotalDislikes = async () => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/count-dislikes?mediaId=${params.movieId}`
+        )
+        setDislikes(response.data)
+    }
+
+    const currentUserRating = async () => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/get-rating?mediaId=${params.movieId}&userId=${userInformation.id}`
+        )
+        console.log(
+            `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/get-rating?mediaId=${params.movieId}&userId=${userInformation.id}`
+        )
+
+        if (response.data.isLiked !== undefined) {
+            //if user likes already, response.data will be TRUE
+            if (response.data.isLiked) {
+                updateLiked(1)
+            } else {
+                updateDisliked(1)
+            }
+        }
+    }
+
+    const saveUserRating = async (userRatingToSave) => {
+        axios
+            .post(
+                `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/save-rating`,
+                {},
+                {
+                    params: {
+                        userId: userInformation.id,
+                        mediaId: params.movieId,
+                        isLiked: userRatingToSave,
+                    },
+                }
+            )
+            .then((response) => response.status)
+            .catch((err) => console.warn(err))
+    }
+
+    const deleteUserRating = async () => {
+        axios
+            .post(
+                `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/delete-rating`,
+                {},
+                {
+                    params: {
+                        userId: userInformation.id,
+                        mediaId: params.movieId,
+                    },
+                }
+            )
+            .then((response) => response.status)
+            .catch((err) => console.warn(err))
+    }
+
+    useEffect(() => {
+        getTotalLikes()
+        getTotalDislikes()
+        currentUserRating()
+    }, [likes, dislikes, liked, disliked])
 
     //checks to see when to increment/decrement likes based on if the like/dislike buttons are pressed
-    const incrementLikes = () => {
+    const clikedLike = () => {
         if (liked) {
             updateLiked(false)
             setLikes(likes - 1)
+
+            deleteUserRating()
         } else {
             updateLiked(true)
             setLikes(likes + 1)
@@ -29,32 +102,31 @@ function LikeDislikes({ likeCount, dislikeCount }) {
                 setLikes(likes + 1)
                 setDislikes(disliked - 1)
             }
+            saveUserRating(true)
         }
-        console.log('plus 1')
 
-        console.log('media Id ' + params.movieId)
-        console.log('user id ' + userInformation.id)
-
-        axios
-            .post(
-                `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/save-rating`,
-                {},
-                {
-                    params: {
-                        userId: userInformation.id,
-                        mediaId: params.movieId,
-                        isLiked: true,
-                    },
-                }
-            )
-            .then((response) => response.status)
-            .catch((err) => console.warn(err))
+        // axios
+        //     .post(
+        //         `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/save-rating`,
+        //         {},
+        //         {
+        //             params: {
+        //                 userId: userInformation.id,
+        //                 mediaId: params.movieId,
+        //                 isLiked: true,
+        //             },
+        //         }
+        //     )
+        //     .then((response) => response.status)
+        //     .catch((err) => console.warn(err))
     }
 
-    const decrementLikes = () => {
+    const clickedDislike = () => {
         if (disliked) {
             updateDisliked(false)
             setDislikes(dislikes - 1)
+
+            deleteUserRating()
         } else {
             updateDisliked(true)
             setDislikes(dislikes + 1)
@@ -63,31 +135,31 @@ function LikeDislikes({ likeCount, dislikeCount }) {
                 setDislikes(dislikes + 1)
                 setLikes(likes - 1)
             }
+            saveUserRating(false)
         }
-        console.log('minus 1')
 
-        axios
-            .post(
-                `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/save-rating`,
-                {},
-                {
-                    params: {
-                        userId: userInformation.id,
-                        mediaId: params.movieId,
-                        isLiked: false,
-                    },
-                }
-            )
-            .then((response) => response.status)
-            .catch((err) => console.warn(err))
+        // axios
+        //     .post(
+        //         `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/save-rating`,
+        //         {},
+        //         {
+        //             params: {
+        //                 userId: userInformation.id,
+        //                 mediaId: params.movieId,
+        //                 isLiked: false,
+        //             },
+        //         }
+        //     )
+        //     .then((response) => response.status)
+        //     .catch((err) => console.warn(err))
     }
 
     return (
         <>
-            <Button className="btnSpacing" onClick={incrementLikes}>
+            <Button className="btnSpacing" onClick={clikedLike}>
                 👍 {likes}
             </Button>
-            <Button className="btnSpacing" onClick={decrementLikes}>
+            <Button className="btnSpacing" onClick={clickedDislike}>
                 👎 {dislikes}
             </Button>
         </>
