6e1f60a4e4df195f28b6d0d4e4787434841093cb
Jonathan Hense <jonathan.hense@gmail.com>
2022-05-18 20:45:03 -0500

    add/remove watchlist entry connected to backend and working

--- src/components/AddWatchlist.js
+++ src/components/AddWatchlist.js
@@ -1,17 +1,69 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
-import React from 'react'
-
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+ 
 function AddWatchlist() {
-    return (
-        <div>
-            <Button className="btnSpacing" variant="success">
-                Add To WatchList
-            </Button>
-            <Button className="btnSpacing" variant="success">
-                Remove From WatchList
-            </Button>
-        </div>
-    )
+   let params = useParams()
+ 
+   const userInformation = useSelector((store) => store.userInformation)
+   const [watchlist, setWatchlist] = useState([])
+   const [watchlistEntry, setWatchlistEntry] = useState({})
+ 
+ 
+   const addToWatchlist = async event => {
+       event.persist()
+       axios
+           .post(`${process.env.REACT_APP_API_BASE_URL}/api/watchlist/add-to-watchlist`,
+               {
+                   user: { id: userInformation.id },
+                   media: { id: params.movieId }
+               })
+          
+           .then((response) => {
+               console.log(response)
+           })
+          
+       setWatchlist([...watchlist, watchlistEntry])
+       event.preventDefault()
+       setWatchlistEntry({})
+      
+   }
+ 
+    const removeFromWatchlist = async event => {
+        event.persist()
+       axios
+           .delete(`${process.env.REACT_APP_API_BASE_URL}/api/watchlist/remove-from-watchlist`,
+            {
+                data: {
+                    user: {id: userInformation.id },
+                    media: { id: params.movieId }
+                }
+ 
+           })
+           .then((response) => {
+               console.log(response)
+           })
+           setWatchlist([...watchlist, watchlistEntry])
+           event.preventDefault()
+           setWatchlistEntry({})
+          
+      
+ 
+   }
+ 
+  
+   return (
+       <div>
+           <Button onClick={addToWatchlist} variant="success">
+               Add To WatchList
+           </Button>
+           <Button onClick={removeFromWatchlist} variant="danger">
+               Remove From WatchList
+           </Button>
+       </div>
+   )
 }
-
+ 
 export default AddWatchlist
---
+++ src/components/Watchlist.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Button } from 'bootstrap'
+import { useParams } from 'react-router-dom'
+
+
+function Watchlist() {
+    let params = useParams()
+
+    const userInformation = useSelector((store) => store.userInformation)
+
+    const [watchlist, setWatchlist] = useState([])
+
+    const displayWatchlist = async () => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/watchlist/get-watchlist?userId=${userInformation.id}`
+        )
+        console.log(
+            `${process.env.REACT_APP_API_BASE_URL}/api/watchlist/get-watchlist?userId=${userInformation.id}`
+        )
+        setWatchlist(response.data)
+    }
+
+    useEffect(() => {
+        displayWatchlist()
+    }
+        , [])
+    
+    return (
+        <div>
+            <h1>Watchlist</h1>
+            <ul>
+                {watchlist.map((item) => (
+                    <li key={item.mediaId}>
+                        {item.media.title}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+    
+
+}
+
