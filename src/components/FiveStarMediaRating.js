030abd88f45e843376ef2ad8ae5d17eb73c85568
Ashlyn Harrington <ashlyn.harrington@gmail.com>
2022-05-22 15:45:22 -0500

    five star rating, formatting media page layout

---
+++ src/components/FiveStarMediaRating.js
@@ -0,0 +1,70 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import 'antd/dist/antd.css'
+import { Rate, Row, Col } from 'antd'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+function FiveStarMediaRating() {
+    const userInformation = useSelector((store) => store.userInformation)
+    let params = useParams()
+
+    const [avgRating, setAvgRating] = useState('')
+    const [rating, updateRating] = useState(2.5)
+    const [displayAvg, updateDisplayAvg] = useState('')
+
+    const getAvgRating = async () => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/get-average-rating-for-media?mediaId=${params.movieId}`
+        )
+        console.log(response.data)
+        if (response.data != null && response.data != 0) {
+            setAvgRating(response.data)
+            updateDisplayAvg('AVG')
+        }
+    }
+
+    const getCurrentUserRating = async () => {
+        const response = await axios.get(
+            `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/get-rating?mediaId=${params.movieId}&userId=${userInformation.id}`
+        )
+        if (response.data !== null) {
+            updateRating(response.data)
+        } else {
+            console.log('USER HAS NOT RATED THIS')
+        }
+    }
+
+    const addNewRating = async (newRatingToSave) => {
+        console.log(newRatingToSave)
+        updateRating(newRatingToSave)
+
+        const response = await axios.post(
+            `${process.env.REACT_APP_API_BASE_URL}/api/media-rating/save-rating?mediaId=${params.movieId}&userId=${userInformation.id}&rating=${newRatingToSave}`
+        )
+        getAvgRating()
+    }
+
+    useEffect(() => {
+        getAvgRating()
+        getCurrentUserRating()
+    }, [])
+
+    return (
+        <Row justify="end" align="middle">
+            <Col span={16}>
+                <Rate
+                    allowHalf
+                    allowClear={true}
+                    defaultValue={2.5}
+                    onChange={addNewRating}
+                />
+            </Col>
+            <Col span={4}>
+                {avgRating} {displayAvg}
+            </Col>
+        </Row>
+    )
+}
+
+export default FiveStarMediaRating
