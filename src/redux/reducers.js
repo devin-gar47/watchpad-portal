844d78374b8aaecfec75ad1a4db4b1042d940d87
Vee Nguyen <veeyn@bu.edu>
2022-05-27 14:01:57 -0400

    Updating section for duration comments to work

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import userSlice from './reducers/user/userSlice'
 import commentSlice from './reducers/comment/commentSlice'
+import durationSlice from './reducers/duration/durationSlice'
 
 const rootReducer = combineReducers({
     userInformation: userSlice,
     mediaComments: commentSlice,
+    durationComments: durationSlice,
 })
 
 export default rootReducer
