4762b64b3e940222ac24b936b33da6dbba36cda1
devin-gar47 <devingar18@gmail.com>
2022-05-28 01:53:01 -0500

    Grabbing user info every time app mounts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import Container from 'react-bootstrap/Container'
 import NavigationBar from './components/NavigationBar'
@@ -13,6 +13,9 @@ import FollowingPage from './pages/profile/FollowingPage'
 import WatchHistoryPage from './pages/profile/WatchHistoryPage'
 import WatchListPage from './pages/profile/WatchListPage'
 import SearchResults from './pages/SearchResults'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { loginSuccess, logout } from './redux/reducers/user/userSlice'
 
 function App() {
     // const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
@@ -20,6 +23,30 @@ function App() {
     //     const newTheme = theme === 'light' ? 'dark' : 'light'
     //     setTheme(newTheme)
     // }
+
+    const userInformation = useSelector((store) => store.userInformation)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        function getUserInfo() {
+            async function getUserInformationApiCall() {
+                if (Object.keys(userInformation).length !== 0) {
+                    const { username } = userInformation
+                    try {
+                        await axios.post(
+                            `${process.env.REACT_APP_API_BASE_URL}/api/user/get-information`,
+                            { username }
+                        )
+                        dispatch(loginSuccess())
+                    } catch (e) {
+                        dispatch(logout())
+                    }
+                }
+            }
+            getUserInformationApiCall()
+        }
+        getUserInfo()
+    }, [])
     return (
         <Container fluid className="p-0">
             <NavigationBar />
