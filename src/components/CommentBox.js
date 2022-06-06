1c28ad43ab93c8f6dc15d6d518055377a59e8352
Ashlyn Harrington <ashlyn.harrington@gmail.com>
2022-06-06 18:44:50 -0500

    Can see trending GIFs, search GIFs, add GIF to review

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Popover, OverlayTrigger } from 'react-bootstrap'
 import './CommentBox.css'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { myAddComment } from '../redux/reducers/comment/commentSlice'
+import GIFModal from './GIFModal'
 
 function CommentBox() {
     let params = useParams()
@@ -12,6 +13,9 @@ function CommentBox() {
 
     const userInformation = useSelector((store) => store.userInformation)
     const [newComment, setNewComment] = useState('') //short term memory to remember what is typed into input area(text area)
+    const [modalShow, setModalShow] = useState(false)
+    const [gifURL, setgifURL] = useState('')
+
     var currentDate = new Date()
     var date =
         currentDate.getFullYear() +
@@ -37,6 +41,7 @@ function CommentBox() {
             content: newComment,
             spoiler: true,
             review: true,
+            gifURL: gifURL,
         }
         axios
             .post(`${process.env.REACT_APP_API_BASE_URL}/api/comments`, comment)
@@ -44,6 +49,7 @@ function CommentBox() {
                 console.log(response.data)
                 if (response.data) {
                     dispatch(myAddComment(response.data))
+                    setgifURL('')
                 } else {
                     console.log('ERROR SAVING COMMENT')
                 }
@@ -58,6 +64,10 @@ function CommentBox() {
     return (
         <div>
             <br />
+
+            <img
+                src={`${gifURL}`} //shows the user's current chosen GIF, if any
+            />
 
             <div className="text-center">
                 <form>
@@ -79,6 +89,22 @@ function CommentBox() {
                     >
                         Post Review
                     </Button>
+
+                    <>
+                        <Button
+                            variant="primary"
+                            onClick={() => setModalShow(true)}
+                        >
+                            +GIF
+                        </Button>
+
+                        <GIFModal
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+                            gifURL={gifURL}
+                            setgifURL={setgifURL}
+                        />
+                    </>
                 </form>
             </div>
             <br />
---
+++ src/components/CommentGIFs.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import '../css/GIFs.css'
+import Loading from './Loading'
+
+const CommentGIFs = (props) => {
+    const [data, setData] = useState([])
+    const [search, setSearch] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+
+    useEffect(() => {
+        async function getTrendingGIFs() {
+            setIsError(false)
+            setIsLoading(true)
+
+            try {
+                const result = await axios(
+                    'https://api.giphy.com/v1/gifs/trending',
+                    {
+                        params: {
+                            api_key: process.env.REACT_APP_GIPHY_APIKEY,
+                            limit: 8,
+                            rating: 'g',
+                        },
+                    }
+                )
+
+                setData(result.data.data)
+                setIsLoading(false)
+                return result
+            } catch (err) {
+                setIsError(true)
+                setIsLoading(false)
+                console.log(`Error getting trending GIFs: ${err}`)
+                setTimeout(() => setIsError(false), 5000)
+            }
+        }
+        getTrendingGIFs()
+    }, [])
+
+    const renderError = () => {
+        if (isError) {
+            return (
+                <div>
+                    <p>Could not load GIFs... Try again later. :(</p>
+                </div>
+            )
+        }
+    }
+
+    const renderGIFs = () => {
+        if (isLoading) {
+            return (
+                <div>
+                    <Loading message="Loading GIFs..." />
+                </div>
+            )
+        }
+        return data.map((gif) => {
+            return (
+                <img
+                    key={gif.id}
+                    className="gif"
+                    src={`${gif.images.fixed_height.url}`}
+                    alt={gif.id}
+                    onClick={() => clickedGIF(gif.images.fixed_height.url)}
+                />
+            )
+        })
+    }
+
+    const clickedGIF = (url) => {
+        props.setgifURL(`${url}`)
+        props.onHide()
+    }
+
+    const handleSearchChange = (event) => {
+        setSearch(event.target.value)
+    }
+
+    const handleSearchSubmit = async (event) => {
+        //prevent page refresh
+        event.preventDefault()
+
+        setIsError(false)
+        setIsLoading(true)
+
+        try {
+            const result = await axios('https://api.giphy.com/v1/gifs/search', {
+                params: {
+                    api_key: process.env.REACT_APP_GIPHY_APIKEY,
+                    limit: 6,
+                    rating: 'g',
+                    lang: 'en',
+                    q: search,
+                },
+            })
+
+            setData(result.data.data)
+            setIsLoading(false)
+            return result
+        } catch (err) {
+            setIsError(true)
+            setIsLoading(false)
+            console.log(`Error trying to search GIFs: ${err}`)
+            setTimeout(() => setIsError(false), 5000)
+        }
+    }
+
+    return (
+        <div>
+            <>{renderError()}</>
+            <form className="form-inline justify-content-center">
+                <input
+                    type="text"
+                    placeholder="search"
+                    onChange={handleSearchChange}
+                    value={search}
+                />
+                <button
+                    type="submit"
+                    className="btn"
+                    onClick={handleSearchSubmit}
+                >
+                    Go
+                </button>
+            </form>
+            <div className="grid">{renderGIFs()}</div>
+        </div>
+    )
+}
+
+export default CommentGIFs
